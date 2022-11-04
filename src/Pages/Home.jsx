5f58ae9153ad39d73aac3dd4b536fa5f1f543d9d
Yeraf <yerafarceguerrero9@gmail.com>
2022-11-04 17:22:42 -0600

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import '../CSS/CssPages/Home.css';
 function Home() {
   return (
     <div className='div-general-pages'>
-        <h1>Home</h1>
+      <div className='container home-div-tittle'>
+      <h1 className='home-h1-title'>Web para todos Alajuelita</h1>
+      </div>
     </div>
   )
 }
