725ca0b1b3d1fa03153c44b03a4ebfdde7fcf058
Yeraf <yerafarceguerrero9@gmail.com>
2022-11-10 17:48:26 -0600

    barber commit

--- src/Pages/Barber.jsx
+++ src/Pages/Barber.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../CSS/CssPages/Barber.css";
+import BarberJson from "../JSON/Barber.json";
 
 function Barber() {
   return (
-    <div className='div-general-pages'>Barber</div>
-  )
+    <div className="container">
+      <div className="div-general-pages">
+        {BarberJson &&
+          BarberJson.map((barberjson) => {
+            return (
+              <div class="card barber-div-card bg-primary" key={barberjson.id}>
+                <img src="..." class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                  <h5 class="card-title">{barberjson.company}</h5>
+                  <p class="card-text">
+                    {barberjson.about}
+                  </p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">An item</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+                </ul>
+                <div class="card-body">
+                  <a href="#" class="card-link">
+                    Card link
+                  </a>
+                  <a href="#" class="card-link">
+                    Another link
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
-export default Barber
+export default Barber;
--- src/Pages/FastFood.jsx
+++ src/Pages/FastFood.jsx
@@ -4,33 +4,35 @@ import Fastfood from "../JSON/Fastfood.json";
 
 function FastFood() {
   return (
-    <div className="div-general-pages container">
+    <div className="container">
+    <div className="div-general-pages">
       { Fastfood && Fastfood.map(fastfood => {
         return (
-          <div class="card fastfood-card" key={fastfood.id}>
-            <img src="..." class="card-img-top" alt="..."></img>
-            <div class="card-body">
-              <h5 class="card-title">{fastfood.company}</h5>
-              <p class="card-text">
+          <div className="card fastfood-card bg-primary" key={fastfood.id}>
+            <img src={fastfood.picture} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">{fastfood.company}</h5>
+              <p className="card-text">
                 {fastfood.about}
               </p>
             </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{fastfood.phone} / {fastfood.phone2}</li>
-              <li class="list-group-item">{fastfood.email}</li>
-              <li class="list-group-item">{fastfood.ubication}</li>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">{fastfood.phone} / {fastfood.phone2}</li>
+              <li className="list-group-item">{fastfood.email}</li>
+              <li className="list-group-item">{fastfood.ubication}</li>
             </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">
+            <div className="card-body">
+              <a href="#" className="card-link">
                 Card link
               </a>
-              <a href="#" class="card-link">
+              <a href="#" className="card-link">
                 Another link
               </a>
             </div>
           </div>
         );
       })}
+    </div>
     </div>
   );
 }
